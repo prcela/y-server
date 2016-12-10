e5ae9a033b5ab85edeab764495d2c5a4de41d33b
prcela <kresimir.prcela@minus5.hr>
2016-12-10 06:33:12 +0100

    test

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "y-server",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
+    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15)
 //        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 6),
